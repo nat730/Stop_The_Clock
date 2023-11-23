626f12cd7e110d71f06d605f7610f588a05a7e7f
jeremy <jeremyfelix41@gmail.com>
2023-11-23 14:42:11 +0100

    [INIT] setup initial.

---
+++ src/client/App.tsx
@@ -0,0 +1,13 @@
+import "./App.scss";
+import { useState } from "react";
+
+function App() {
+  //const [count, setCount] = useState(0);
+
+  return (
+    <>    
+    </>
+  );
+}
+
+export default App;
--- src/server/main.ts
+++ src/server/main.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import ViteExpress from "vite-express";
+import "dotenv/config"
 
 const app = express();
+const port = process.env.PORT
 
 app.get("/hello", (_, res) => {
   res.send("Hello Vite + React + TypeScript!");
 });
 
 ViteExpress.listen(app, 3000, () =>
-  console.log("Server is listening on port 3000...")
+  console.log("Server is listening on port " + port)
 );
