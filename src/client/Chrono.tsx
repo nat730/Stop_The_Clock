81da106608455036efed843014745edc7aadc422
nat730 <natanaelcolart@gmail.com>
2023-11-23 16:33:09 +0100

    jeu fonctionne (besoin rajout chrono)

--- src/client/Chrono.tsx
+++ src/client/Chrono.tsx
@@ -6,15 +6,18 @@ function Chrono() {
   const [isRunning, setIsRunning] = useState<boolean>(false);
   const [elapsedTime, setElapsedTime] = useState<number>(0);
 
-  useEffect(() => { 
+  useEffect(() => {
     let interval: NodeJS.Timeout;
-
+  
     if (isRunning) {
+      setElapsedTime(0);
+      let startTime = Date.now();
+  
       interval = setInterval(() => {
-        setElapsedTime((prevTime) => prevTime + 1);
+        setElapsedTime((Date.now() - startTime) / 1000);
       }, 1000);
     }
-
+  
     return () => clearInterval(interval);
   }, [isRunning]);
 
@@ -25,14 +28,19 @@ function Chrono() {
   const resetGame = () => {
     setIsRunning(false);
     setElapsedTime(0);
-    setTargetNumber(Math.floor(Math.random() * 20) + 1);
+    setTargetNumber(Math.floor(Math.random() * 10) + 1);
   };
+
+  let difference = elapsedTime - targetNumber;
 
   return (
     <div className="App">
-      <h1>Stop Chrono</h1>
+      <h1>Arrêtez le Chronomètre</h1>
       <p>Nombre cible: {targetNumber}</p>
-      <p>Temps écoulé: {elapsedTime} secondes</p>
+      {!isRunning && <p>Temps écoulé: {elapsedTime} secondes</p>}
+      {!isRunning && difference === 0 && <p>Vous avez parfaitement atteint le nombre, bravo !</p>}
+      {(!isRunning && difference > -1 && difference < 1 && difference !== 0) && <p>Vous avez presque atteint l'objectif, bravo !</p>}
+      {(!isRunning && (difference <= -1 || difference >= 1) && difference !== 0) && <p>dommage réesssaye la prochaine fois</p>}
       <button onClick={startStopGame}>{isRunning ? 'Arrêter' : 'Démarrer'}</button>
       <button onClick={resetGame}>Réinitialiser</button>
     </div>
